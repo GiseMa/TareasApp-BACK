63ebf5d742c47b36e782083aecd76fb43ae7d22a
GiseMa <gisellemaravankin7@hotmail.com>
2025-09-18 23:12:42 -0300

    Cambie los checks de validacion a otro archivo

--- middlewares/validateFields.js
+++ middlewares/validateFields.js
@@ -1,4 +1,6 @@
 import { validationResult } from "express-validator";
+import { check } from "express-validator"
+import { isDate } from "util/types";
 
 export const validateFields = (req, res, next) => {
 
@@ -13,3 +15,27 @@ export const validateFields = (req, res, next) => {
     
     next();
 }
+
+export const registerValidation = [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El mail es obligatorio').isEmail(),
+    check('password', 'La contrasenia debe tener por lo menos 6 caracteres').isLength({min: 6}),
+];
+
+export const loginValidation = [
+    check('email', 'El mail es obligatorio').isEmail(),
+    check('password', 'La contrasenia debe tener por lo menos 6 caracteres').isLength({min: 6}),
+];
+
+export const createValidation = [
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('text', 'Las notas son obligatorias').not().isEmpty(),
+    check('type', 'Tiene que elegir un tipo').not().isEmpty(),
+    check('start', 'Fecha de inicio obligatoria').custom(isDate),
+];
+
+export const updateValidation = [
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio obligatoria').custom(isDate),
+
+];
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,31 +1,13 @@
 import { Router } from "express";
-import { check } from "express-validator";
-import {validateFields}  from "../middlewares/validateFields.js";
+import {loginValidation, registerValidation, validateFields}  from "../middlewares/validateFields.js";
 import { createUser, loginUser, renewToken } from "../controllers/auth.js";
 import { validateJWT } from "../middlewares/validateJWT.js";
 
 const authRoutes = Router();
 
-authRoutes.post(
-    '/register',
-    [
-        check('name', 'El nombre es obligatorio').not().isEmpty(),
-        check('email', 'El mail es obligatorio').isEmail(),
-        check('password', 'La contrasenia debe tener por lo menos 6 caracteres').isLength({min: 6}),
-        validateFields
-    ],
-    createUser
-);
+authRoutes.post('/register', registerValidation, validateFields, createUser);
 
-authRoutes.post(
-    '/login',
-    [
-        check('email', 'El mail es obligatorio').isEmail(),
-        check('password', 'La contrasenia debe tener por lo menos 6 caracteres').isLength({min: 6}),
-        validateFields
-    ],
-    loginUser
-);
+authRoutes.post('/login', loginValidation, validateFields, loginUser);
 
 authRoutes.get('/renew', validateJWT, renewToken);
 
