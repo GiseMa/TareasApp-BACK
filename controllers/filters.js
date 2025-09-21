35543220a302787f2c49205bcf48441e3f27cc1d
GiseMa <gisellemaravankin7@hotmail.com>
2025-09-20 21:42:43 -0300

    Cambio de llamada de filtros a BD

--- controllers/filters.js
+++ controllers/filters.js
@@ -3,10 +3,17 @@ import Filter from "../models/Filter.js";
 export const getFilters = async(req, res) => {
 
     try {
-        const types = await Filter.find({filterType: 'type'});
+        //$in es un operador de comparacion de mongoDB
+        const filters = await Filter.find({filterType: {$in: ['type', 'state', 'order']}});
+        
+        const types = filters.filter(f => f.filterType === 'type');
+        const state = filters.filter(f => f.filterType === 'state');
+        const order = filters.filter(f => f.filterType === 'order');
+
+       /*  const types = await Filter.find({filterType: 'type'});
         const state = await Filter.find({filterType: 'state'});
         const order = await Filter.find({filterType: 'order'});
-
+        */
         res.json({
             types, state, order
         });
